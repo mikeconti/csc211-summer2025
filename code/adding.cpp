10b9857a115002d4c01f38a1ddbb8ec907127249
Michael Conti <michaelconti@uri.edu>
2025-06-25 13:02:59 -0400

    slides

--- code/adding.cpp
+++ code/adding.cpp
@@ -10,6 +10,8 @@ int main(){
     
     std::cout << "The values a + b = " << a + b;
     std::cout << "All Done" << a + b;
+    std::cout << "All Done, for real this time" << a + b;
+
 
     return 0;
 }
